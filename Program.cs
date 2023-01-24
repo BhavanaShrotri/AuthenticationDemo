237e282bc9d86553d094d16043fc6f5d2e556a27
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-01-24 15:24:01 +0530

    created AuthService

--- Program.cs
+++ Program.cs
@@ -1,34 +1,66 @@
 using Microsoft.AspNetCore.DataProtection;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
+app.Use((ctx, next) =>
 {
-
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = idp.CreateProtector("auth-cookie");
-
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
     var protectedPayload = authCookie.Split("=").Last();
     var payload = protector.Unprotect(protectedPayload);
     var parts = payload.Split(':');
     var key = parts[0];
     var value = parts[1];
-    return value;
 
+    var claims = new List<Claim>();
+    claims.Add(new Claim(key, value));
+    var identity = new ClaimsIdentity(claims);
+    ctx.User = new ClaimsPrincipal(identity);
+
+    return next();
+});
+
+
+app.MapGet("/username", (HttpContext ctx) =>
+{
+
+    return ctx.User.FindFirstValue("usr");
     // return "Bhavana";
 });
 
 
-app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
+app.MapGet("/login", (AuthService auth) =>
 {
-    var protector = idp.CreateProtector("auth-cookie");
-    ctx.Response.Headers["Set-Cookie"] = $"auth={protector.Protect("usr:Bhavana")}";
+    auth.SignIn();
     return "ok";
 });
 
 
 app.Run();
+
+
+public class AuthService
+{
+    private readonly IDataProtectionProvider _idp;
+    private readonly IHttpContextAccessor _accessor; // stores the http request and resopnse
+
+    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+    {
+        _idp = idp;
+        _accessor = accessor;
+    }
+
+    public void SignIn()
+    {
+        var protector = _idp.CreateProtector("auth-cookie");
+        _accessor.HttpContext.Response.Headers["Set-Cookie"] = $"auth={protector.Protect("usr:Bhavana")}";
+    }
+}
